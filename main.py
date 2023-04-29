4e6cb0d9df955718397d36795f5ec34c55e61c52
Miguel Lopez <ing.miguell@gmail.com>
2023-04-28 18:02:25 -0600

    add functionality to button add and save the entry in txt file

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 
-FONT_NAME = "Ariel"
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    new_entry = f"{website} | {username} | {password}"
+
+    with open(file="data.txt", mode="a") as data_file:
+        data_file.write(new_entry + "\n")
+
+    website_entry.delete(0, last="end")
+    password_entry.delete(0, last="end")
+
+    website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -19,25 +31,27 @@ canvas.grid(column=1, row=0)
 website_label = tk.Label(text="Website:", bg="white", fg="black")
 website_label.grid(column=0, row=1)
 
-website_entry = tk.Entry(width=38, bg="white", fg="black", highlightthickness=0)
+website_entry = tk.Entry(width=38, bg="white", fg="black", highlightthickness=0, insertbackground="black")
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 username_label = tk.Label(text="Email/Username:", bg="white", fg="black")
 username_label.grid(column=0, row=2)
 
-username_entry = tk.Entry(width=38, bg="white", fg="black", highlightthickness=0)
+username_entry = tk.Entry(width=38, bg="white", fg="black", highlightthickness=0, insertbackground="black")
 username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(index="end", string="example@example.com")
 
 password_label = tk.Label(text="Password:", bg="white", fg="black")
 password_label.grid(column=0, row=3)
 
-password_entry = tk.Entry(width=21, bg="white", fg="black", highlightthickness=0)
+password_entry = tk.Entry(width=21, bg="white", fg="black", highlightthickness=0, insertbackground="black")
 password_entry.grid(column=1, row=3)
 
 generate_password_button = tk.Button(text="Generate Password", highlightbackground="white")
 generate_password_button.grid(column=2, row=3)
 
-add_button = tk.Button(text="Add", width=36, highlightbackground="white")
+add_button = tk.Button(text="Add", width=36, highlightbackground="white", command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
